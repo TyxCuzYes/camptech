3f54fad9f74fc13f75b031be45fdb99873d2ad01
TyxCuzYes <roobloox.player@gmail.com>
2022-07-09 15:41:21 +0530

    Commit

---
+++ src/Pages/Help.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+function Help(){
+    return(
+        <div className="Pages">
+            <div className="Contact">
+                <i class="ri-mail-fill"></i>
+                <span>ankush.chakraborty138@gmail.com</span>
+            </div>
+            <div className="Contact">
+                <i class="ri-phone-fill"></i>
+                <span>9433377637</span>
+            </div>
+            <h1>Support</h1>
+            <br/>
+            <p>dk dkd dk dkd kd dk</p>
+            <br/>
+            <button className="Help">Learn more</button>
+        </div>
+    );
+}
+
+export default Help;
